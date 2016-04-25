73f875b74b44394e1c8021f97c6368bc4cb7e672
preety <preety@ccs.neu.edu>
2016-04-25 12:01:08 -0700

    Minor calculation change

--- www/js/services.js
+++ www/js/services.js
@@ -3,17 +3,17 @@
     'use strict';
 
     var app = angular.module('aqx');
-    
+
     var host = 'http://127.0.0.1:5000'
     var endpoint = host + '/aqxapi/v2/';
     var USE_MOCK = true;
-    
+
     app.factory('UserService', function($http, $q) {
-        
+
         var service = {
             getUser: getUser
         };
-        
+
         function getGoogleProfile(accessToken) {
             var deferred = $q.defer();
             function onSuccess(response) {
@@ -26,7 +26,7 @@
             $http.get(url).then(onSuccess, onFailure);
             return deferred.promise;
         }
-        
+
         function getUser(accessToken) {
             var deferred = $q.defer();
             var user = {};
@@ -40,9 +40,9 @@
                     deferred.resolve(user);
                 }
                 else {
-                    $http.get(endpoint + 'user/' + googleID).then(onSuccess2, onFailure);    
+                    $http.get(endpoint + 'user/' + googleID).then(onSuccess2, onFailure);
                 }
-                
+
             }
             function onSuccess2(response) {
                 user.ID = response.data.user.id;
@@ -54,66 +54,72 @@
             getGoogleProfile(accessToken).then(onSuccess, onFailure);
             return deferred.promise;
         }
-        
+
         return service;
     });
-    
+
     app.factory('LightMeterService', function ($q) {
-        
+
         var service = {
             computeLux: computeLux
         }
-        
+
         function computeLux(imageURI, exif) {
             var deferred = $q.defer();
-            
+
             var exif = JSON.parse(exif).Exif;
             var ISO = exif.ISOSpeedRatings;
             var exposure = exif.ExposureTime;
             var brightnessValue = exif.BrightnessValue;
-            
+
             var image = document.createElement('img');
             var context = document.createElement('canvas').getContext('2d');
             image.src = imageURI;
-            
+
             var illuminance = 7500;
-            
-            var normalAverage = 0.0;
             var grayscale = 0.0;
-            var imageBrightnessStd = 0.0;
-            var imageBrightnessNonLinear = 0.0;
-            
+//            var normalAverage = 0.0;
+//            var imageBrightnessStd = 0.0;
+//            var imageBrightnessNonLinear = 0.0;
+
             image.onload = function() {
                 var width = image.naturalWidth;
                 var height = image.naturalHeight;
                 context.drawImage(image, 0, 0, width, height);
                 var data = context.getImageData(0, 0, width, height).data;
                 var r, g, b, a;
-                var pixelCount = data.length / 4;
-                
+                var pixelCount = 0.0;
+                var totalIlluminance = 0.0;
+
                 for (var i = 0; i < data.length; i += 4) {
-                    
+
                     r = data[i];
                     g = data[i + 1];
                     b = data[i + 2];
-                    
-                    // normalAverage += (r + g + b) / 3.0;
+
                     grayscale += 0.299 * r + 0.587 * g + 0.114 * b;
+                    // normalAverage += (r + g + b) / 3.0;
                     // imageBrightnessStd += 0.2126 * r + 0.7152 * g + 0.0722 * b;
                     // imageBrightnessNonLinear += Math.sqrt( 0.241 * Math.pow(r, 2) + 0.691 * Math.pow(g, 2) + 0.068 * Math.pow(b, 2) );
                     pixelCount += 1;
                 }
-                
-                // averageBrightness += (normalAverage + grayscale + imageBrightnessStd + imageBrightnessNonLinear) / (4 * pixelCount);
+
                 var averageBrightness = grayscale / pixelCount;
-                var totalIlluminance = illuminance * Math.pow(averageBrightness, 2) / (0.0929 * ISO / exposure);
-                
-                deferred.resolve(totalIlluminance);  
+
+                if(brightnessValue > 4) {
+                  totalIlluminance = illuminance * Math.pow(brightnessValue, 2) / (ISO * 0.25083);
+                  console.log("totalIlluminance of bright surrounding::  " + totalIlluminance);
+                } else {
+                  totalIlluminance = Math.pow(brightnessValue, 2) / (0.0929);
+                  console.log("totalIlluminance of dark surrounding::  " + totalIlluminance);
+                }
+
+                deferred.resolve(totalIlluminance);
             }
-            
+
             return deferred.promise;
         }
-        
+
         return service;
     });
 
@@ -126,7 +132,7 @@
             submitReading: submitReading,
             submitAnnotation: submitAnnotation
         };
-        
+
         var measurements = [
             {
                 name: 'Alkalinity',
@@ -223,7 +229,7 @@
                         ID: '3',
                         name: 'Test3'
                     }
-                ]);    
+                ]);
             }
             else {
                 $http.get(endpoint + 'user/' + userID + '/system').then(onSuccess, onFailure);
@@ -258,40 +264,40 @@
                     name: systemUID,
                     readings: readings
                 };
-                onSuccess(system);    
+                onSuccess(system);
             }
             else {
-                $http.get(endpoint + 'system/' + systemUID)    
+                $http.get(endpoint + 'system/' + systemUID)
             }
             return deferred.promise;
         }
-        
+
         function getReadingsForSystem(systemUID) {
             function onSuccess(response) {
-                
+
             }
             function onFailure(error) {
-                
+
             }
             // $http.get(endpoint + 'system/' + systemUID + '/reading').then(onSuccess, onResponse);
         }
 
         function submitReading(systemUID, type, reading) {
             function onSuccess(response) {
-                
+
             }
             function onFailure(error) {
-                
+
             }
             // $http.post(endpoint + 'system/' + systemUID + '/reading/' + type, reading).then(onSuccess, onFailure);
         }
-        
+
         function submitAnnotation(systemID, annotation) {
             function onSuccess(response) {
-                
+
             }
             function onFailure(error) {
-                
+
             }
             // $http.post(endpoint + 'system/' + systemID + '/annotation', annotation).then(onSuccess, onFailure);
         }
